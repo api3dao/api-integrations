ab890862c304e84bff6a02c6170a04acd8efab96
metobom <unutkan2013@yandex.com>
2023-10-23 15:30:41 +0300

    Update pre-commit hook & prettier

--- src/config-generation/config-utils.ts
+++ src/config-generation/config-utils.ts
@@ -1,8 +1,7 @@
 import * as fs from 'fs';
-import * as crypto from "crypto";
-import { ethers } from "ethers";
-import { encode } from "@api3/airnode-abi";
-
+import * as crypto from 'crypto';
+import { ethers } from 'ethers';
+import { encode } from '@api3/airnode-abi';
 
 export function deriveDataFeedId(oisTitle: string, dapiName: string, airnodeAddress: string) {
   const templateId = deriveTemplateId(oisTitle, dapiName);
@@ -11,15 +10,13 @@ export function deriveDataFeedId(oisTitle: string, dapiName: string, airnodeAddr
 
 export function deriveTemplateId(oisTitle: string, dapiName: string) {
   const endpointId = deriveEndpointId(oisTitle);
-  const parameters = encode([{ "name": "name", "type": "string32", "value": dapiName }]);
+  const parameters = encode([{ name: 'name', type: 'string32', value: dapiName }]);
   return ethers.utils.solidityKeccak256(['bytes32', 'bytes'], [endpointId, parameters]);
 }
 
 export function deriveEndpointId(oisTitle: string) {
-  const endpointName = "feed";
-  return ethers.utils.keccak256(
-    ethers.utils.defaultAbiCoder.encode(['string', 'string'], [oisTitle, endpointName])
-  );
+  const endpointName = 'feed';
+  return ethers.utils.keccak256(ethers.utils.defaultAbiCoder.encode(['string', 'string'], [oisTitle, endpointName]));
 }
 
 export function readJson(path: string) {
@@ -33,7 +30,7 @@ export function saveJson(path: string, jsonObject: object) {
 }
 
 export function mkdirIfDoesntExists(path: string) {
-  if(!fs.existsSync(path)) {
+  if (!fs.existsSync(path)) {
     fs.mkdirSync(path);
   }
 }
@@ -41,27 +38,31 @@ export function mkdirIfDoesntExists(path: string) {
 export function deriveDeploymentId(configGenerationTimestamp: number, airnodeAddress: string) {
   const DEPLOYMENT_ID_LENGTH = 8;
   return crypto
-  .createHash("sha256")
-  .update([configGenerationTimestamp, airnodeAddress, ].join(''))
-  .digest("hex")
-  .substring(0, DEPLOYMENT_ID_LENGTH);
+    .createHash('sha256')
+    .update([configGenerationTimestamp, airnodeAddress].join(''))
+    .digest('hex')
+    .substring(0, DEPLOYMENT_ID_LENGTH);
 }
 
 // if contents of preProcessing change this function might require changes in .replace (!!!)
 export function extractPreProcessingObject(ois: any) {
-  const feedEndpoint = ois.endpoints.find((e: any) => e.name === "feed");
+  const feedEndpoint = ois.endpoints.find((e: any) => e.name === 'feed');
   const preProcValue = feedEndpoint.preProcessingSpecifications[0].value
-  .replace("path: preProcessingObject[endpointParameters.name].path,", "")
-  .replace("...preProcessingObject[endpointParameters.name].parameters,", "");
-  const evaluated = Function("endpointParameters", "input", "output", preProcValue.replace("output = parser(input)", "") + "return preProcessingObject;");
-  return evaluated({name: ""}, "", "");
+    .replace('path: preProcessingObject[endpointParameters.name].path,', '')
+    .replace('...preProcessingObject[endpointParameters.name].parameters,', '');
+  const evaluated = Function(
+    'endpointParameters',
+    'input',
+    'output',
+    preProcValue.replace('output = parser(input)', '') + 'return preProcessingObject;'
+  );
+  return evaluated({ name: '' }, '', '');
 }
 
 // if contents of postProcessing change this function might require changes in .replace (!!!)
 export function extractPostProcessingObject(ois: any) {
-  const feedEndpoint = ois.endpoints.find((e: any) => e.name === "feed");
-  const postProcValue = feedEndpoint.postProcessingSpecifications[0].value
-  .replace("output = parser(input)", "");
-  const evaluated = Function("endpointParameters", "input", "output", postProcValue + "return postProcessingObject;");
-  return evaluated({name: ""}, "", "");
+  const feedEndpoint = ois.endpoints.find((e: any) => e.name === 'feed');
+  const postProcValue = feedEndpoint.postProcessingSpecifications[0].value.replace('output = parser(input)', '');
+  const evaluated = Function('endpointParameters', 'input', 'output', postProcValue + 'return postProcessingObject;');
+  return evaluated({ name: '' }, '', '');
 }
--- src/config-generation/pusher/generate-deployment.ts
+++ src/config-generation/pusher/generate-deployment.ts
@@ -1,17 +1,23 @@
-import { join } from "path";
-import { globSync } from "glob";
-import { ethers } from "ethers";
-import { Logger, ILogObj } from "tslog";
-import { deriveEndpointId, deriveTemplateId, readJson, saveJson, deriveDeploymentId, extractPreProcessingObject, extractPostProcessingObject } from "../config-utils";
+import { join } from 'path';
+import { globSync } from 'glob';
+import { ethers } from 'ethers';
+import { Logger, ILogObj } from 'tslog';
+import {
+  deriveEndpointId,
+  deriveTemplateId,
+  readJson,
+  saveJson,
+  deriveDeploymentId,
+  extractPreProcessingObject,
+  extractPostProcessingObject
+} from '../config-utils';
 
-import * as fs from "fs";
+import * as fs from 'fs';
 
-const prompts = require("prompts");
-
+const prompts = require('prompts');
 
 const main = async () => {
-  
-  const APIS_ROOT = "./data/apis/";
+  const APIS_ROOT = './data/apis/';
   const configGenerationTimestamp = Math.floor(Date.now() / 1000);
 
   const logger: Logger<ILogObj> = new Logger();
@@ -19,36 +25,42 @@ const main = async () => {
   // get the apiName
   const existingProviders = fs.readdirSync(APIS_ROOT);
   const { apiName } = await prompts({
-    type: "select",
-    name: "apiName",
-    message: "Select an API to create deployment:",
+    type: 'select',
+    name: 'apiName',
+    message: 'Select an API to create deployment:',
     choices: existingProviders.map((apiName) => {
-      return { title: apiName, value: apiName }
+      return { title: apiName, value: apiName };
     })
   });
 
   // read required files
-  let pusherConfig = readJson("./boilerplates/boilerplate-pusher-config.json");
+  let pusherConfig = readJson('./boilerplates/boilerplate-pusher-config.json');
   const apiData = readJson(`./data/apis/${apiName}/api-data.json`);
-  
+
   // read oises
   const oises = globSync(`./data/apis/${apiName}/oises/*`).map((oisPath) => readJson(oisPath));
 
   // init "signedApiUpdates" triggers
-  pusherConfig.triggers["signedApiUpdates"] = [];
+  pusherConfig.triggers['signedApiUpdates'] = [];
 
   // generate rateLimiting object & push OIS objects
   oises.map((ois) => {
-    pusherConfig.rateLimiting[ois.title] = { "maxConcurrency": 25, "minTime": 10 };
+    pusherConfig.rateLimiting[ois.title] = { maxConcurrency: 25, minTime: 10 };
     pusherConfig.ois.push(ois);
-  })
+  });
 
   // validate OIS titles in "apiData.supportedFeedsInBatches"
-  const originalOisTitlesHash = ethers.utils.solidityKeccak256(oises.map((o) => "string"), oises.map((o) => o.title).sort());
-  const oisTitlesHashFromApiData = ethers.utils.solidityKeccak256(Object.keys(apiData.supportedFeedsInBatches).map((ot) => "string"), Object.keys(apiData.supportedFeedsInBatches).sort());
-  if(originalOisTitlesHash !== oisTitlesHashFromApiData) {
-    logger.error("Actual OIS titles and OIS titles in api-data.json are not same!");
-    logger.error("Exiting...");
+  const originalOisTitlesHash = ethers.utils.solidityKeccak256(
+    oises.map((o) => 'string'),
+    oises.map((o) => o.title).sort()
+  );
+  const oisTitlesHashFromApiData = ethers.utils.solidityKeccak256(
+    Object.keys(apiData.supportedFeedsInBatches).map((ot) => 'string'),
+    Object.keys(apiData.supportedFeedsInBatches).sort()
+  );
+  if (originalOisTitlesHash !== oisTitlesHashFromApiData) {
+    logger.error('Actual OIS titles and OIS titles in api-data.json are not same!');
+    logger.error('Exiting...');
     process.exit();
   }
 
@@ -60,9 +72,9 @@ const main = async () => {
     const preProcessingObject = extractPreProcessingObject(ois);
     const feedsInPreProcessingObject = Object.keys(preProcessingObject);
     const mismatchPreSupp = feedsInPreProcessingObject
-    .filter((feedName: string) => !supportedFeeds.includes(feedName))
-    .concat(supportedFeeds.filter((feedName: string) => !feedsInPreProcessingObject.includes(feedName)));
-    if(mismatchPreSupp.length !== 0) {
+      .filter((feedName: string) => !supportedFeeds.includes(feedName))
+      .concat(supportedFeeds.filter((feedName: string) => !feedsInPreProcessingObject.includes(feedName)));
+    if (mismatchPreSupp.length !== 0) {
       logger.error(`Mismatch between supported feeds and preProcessingObject >> ${JSON.stringify(mismatchPreSupp)}`);
     }
 
@@ -70,31 +82,31 @@ const main = async () => {
     const postProcessingObject = extractPostProcessingObject(ois);
     const feedsInPostProcessingObject = Object.keys(postProcessingObject);
     const mismatchPostSupp = feedsInPostProcessingObject
-    .filter((feedName: string) => !supportedFeeds.includes(feedName))
-    .concat(supportedFeeds.filter((feedName: string) => !feedsInPostProcessingObject.includes(feedName)));
-    if(mismatchPostSupp.length !== 0) {
+      .filter((feedName: string) => !supportedFeeds.includes(feedName))
+      .concat(supportedFeeds.filter((feedName: string) => !feedsInPostProcessingObject.includes(feedName)));
+    if (mismatchPostSupp.length !== 0) {
       logger.error(`Mismatch between supported feeds and postProcessingObject >> ${JSON.stringify(mismatchPostSupp)}`);
     }
 
     // validate preProcessingObject & postProcessingObject (might not be necessary)
     const mismatchPrePost = feedsInPostProcessingObject
-    .filter((feedName: string) => !feedsInPreProcessingObject.includes(feedName))
-    .concat(feedsInPreProcessingObject.filter((feedName: string) => !feedsInPostProcessingObject.includes(feedName)));
-    if(mismatchPostSupp.length !== 0) {
-      logger.error(`Mismatch between postProcessingObject and preProcessingObject >> ${JSON.stringify(mismatchPrePost)}`);
+      .filter((feedName: string) => !feedsInPreProcessingObject.includes(feedName))
+      .concat(feedsInPreProcessingObject.filter((feedName: string) => !feedsInPostProcessingObject.includes(feedName)));
+    if (mismatchPostSupp.length !== 0) {
+      logger.error(
+        `Mismatch between postProcessingObject and preProcessingObject >> ${JSON.stringify(mismatchPrePost)}`
+      );
     }
-
   });
-  
 
   // generate templates
   Object.keys(apiData.supportedFeedsInBatches).map((oisTitle) => {
     // add endpoints
     const endpointId = deriveEndpointId(oisTitle);
     pusherConfig.endpoints[endpointId] = {
-      endpointName: "feed",
+      endpointName: 'feed',
       oisTitle: oisTitle
-    }
+    };
 
     // add templates
     apiData.supportedFeedsInBatches[oisTitle].map((batch: string[]) => {
@@ -102,48 +114,43 @@ const main = async () => {
         const templateId = deriveTemplateId(oisTitle, feedName);
         pusherConfig.templates[templateId] = {
           endpointId: endpointId,
-          parameters: [{ type: "string32", name: "name", value: feedName }]
-        }
+          parameters: [{ type: 'string32', name: 'name', value: feedName }]
+        };
         return templateId;
-      })
+      });
 
       // add triggers
-      pusherConfig.triggers["signedApiUpdates"].push(
-        {
-          signedApiName: "Nodary",
-          templateIds: templateIds,
-          fetchInterval: 5,
-          updateDelay: 30
-        }
-      )
-      
+      pusherConfig.triggers['signedApiUpdates'].push({
+        signedApiName: 'Nodary',
+        templateIds: templateIds,
+        fetchInterval: 5,
+        updateDelay: 30
+      });
     });
-  })
-  
+  });
+
   // generate signedApis
   pusherConfig.signedApis = [
     {
-      name: "Nodary",
-      url: "https://pool.nodary.io"
+      name: 'Nodary',
+      url: 'https://pool.nodary.io'
     }
-  ]
+  ];
 
   // generate apiCredentials
   pusherConfig.apiCredentials = [];
   oises.map((ois) => {
     Object.keys(ois.apiSpecifications.components.securitySchemes).map((schemeName) => {
-      pusherConfig.apiCredentials.push(
-        {
-          oisTitle: ois.title,
-          securitySchemeName: schemeName,
-          securitySchemeValue: "${"+`${schemeName.toUpperCase()}_VALUE`+"}"
-        }
-      );
+      pusherConfig.apiCredentials.push({
+        oisTitle: ois.title,
+        securitySchemeName: schemeName,
+        securitySchemeValue: '${' + `${schemeName.toUpperCase()}_VALUE` + '}'
+      });
     });
   });
-  
+
   // derive deployment id
-  const deploymentId = deriveDeploymentId(configGenerationTimestamp, apiData.airnodeAddress)
+  const deploymentId = deriveDeploymentId(configGenerationTimestamp, apiData.airnodeAddress);
   pusherConfig.deploymentId = deploymentId;
 
   // save the deployment
@@ -151,7 +158,6 @@ const main = async () => {
   saveJson(join(deploymentPath, `${deploymentId}-pusher.json`), pusherConfig);
 
   logger.info(`Generated deployment for ${apiName} with name ${deploymentId}-pusher.json.`);
-}
-
+};
 
 main();
