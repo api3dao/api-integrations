b873d1bb45f9b322aa910092c66ee1ad3fd41ac5
hiletmis <hiletmis@gmail.com>
2023-10-24 17:10:08 +0300

    deployment category view

---
+++ frontend/src/Custom/DeploymentCategory.js
@@ -0,0 +1,23 @@
+import ExpandableView from "./ExpandableView";
+import IntegrationRow from "../Components/IntegrationRow";
+
+const DeploymentCategory = ({ header, integrations }) => {
+    return (
+        <ExpandableView
+            header={header}
+            defaultState={true}
+            view={
+                (
+                    integrations.length === 0 ?
+                        <IntegrationRow integration={null} />
+                        :
+                        integrations.map((integration, index) => (
+                            <IntegrationRow key={index} integration={integration} />
+                        ))
+                )
+            }
+        />
+    )
+};
+
+export default DeploymentCategory;
