a10129ca5fe8f11a6a0fe68354685b1c96696110
hiletmis <hiletmis@gmail.com>
2023-10-25 15:21:44 +0300

    compare ids added

--- frontend/src/Components/Integrations.js
+++ frontend/src/Components/Integrations.js
@@ -1,18 +1,15 @@
-import { VStack, Flex } from "@chakra-ui/react";
+import { VStack, Flex, Text, Stack } from "@chakra-ui/react";
 import { COLORS } from "../data/constants";
 import Display from "./Display";
 import CompareEndpoints from "./CompareEndpoints";
 import { useContext } from "react";
 import { ApiIntegrationsContext } from "../Context"
 import DeploymentCategory from "../Custom/DeploymentCategory";
+import CompareId from "../Custom/CompareId";
 
 const Integrations = ({ integrations }) => {
 
-  const { config, comparePair } = useContext(ApiIntegrationsContext);
-
-  const isComparePairFull = () => {
-    return comparePair.left !== null && comparePair.right !== null;
-  }
+  const { config, comparePair, setComparePair } = useContext(ApiIntegrationsContext);
 
   return (
     <Flex spacing={4} overflow={"scroll"}>
@@ -34,16 +31,24 @@ const Integrations = ({ integrations }) => {
           {config === null ?
             <>
               {
-                isComparePairFull() ?
+                comparePair.left === null && comparePair.right === null ? null :
+                  <Stack direction={"row"} justifyContent={"center"}>
+                    <CompareId header={"New"} onClick={() => { setComparePair({ left: null, right: comparePair.right }) }} text={comparePair.left == null ? "Click on a provider to compare it with another" : comparePair.left.stage} />
+                    <Flex alignItems={"center"} justifyItems={"center"} >
+                      <Text align={"center"} fontSize={"sm"} fontWeight={"bold"} color={"gray.500"} >VS</Text>
+                    </Flex>
+                    <CompareId header={"Old"} onClick={() => { setComparePair({ left: comparePair.left, right: null }) }} text={comparePair.right == null ? "Click on a provider to compare it with another" : comparePair.right.stage} />
+                  </Stack>
+              }
+              {
+                comparePair.left !== null && comparePair.right !== null ?
                   <CompareEndpoints oldOis={comparePair.left.ois} newOis={comparePair.right.ois} />
                   :
                   <>
                     <DeploymentCategory header={"Active Deployments"} integrations={integrations.activeDeployment} />
                     <DeploymentCategory header={"Candidate Deployments"} integrations={integrations.candidateDeployment} />
                   </>
-
               }
-
             </>
             :
             <Display configData={config} />}
