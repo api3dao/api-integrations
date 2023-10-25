60e9634e8fe6a540eb57382c6e5e97b1227f59af
hiletmis <hiletmis@gmail.com>
2023-10-25 15:21:14 +0300

    compare fix

--- frontend/src/Components/CompareEndpoints.js
+++ frontend/src/Components/CompareEndpoints.js
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react";
-import { VStack } from "@chakra-ui/react";
+import { VStack, Text } from "@chakra-ui/react";
 import { extractFeeds } from "../Helpers/Utils";
 import FeedGroupView from "../Custom/FeedGroupView";
 import FeedCompareGroupView from "../Custom/FeedCompareGroupView";
+import { COLORS } from "../data/constants";
 
-const Endpoint = ({ oldOis, newOis }) => {
+const CompareEndpoints = ({ oldOis, newOis }) => {
   const [comparison, setComparison] = useState(null);
 
   useEffect(() => {
@@ -14,58 +15,72 @@ const Endpoint = ({ oldOis, newOis }) => {
     if (newOis.length === 0) return;
 
     const comparison = extractFeeds(oldOis, newOis);
+
     setComparison(comparison);
   }, [newOis, oldOis]);
 
   return oldOis == null || newOis === null ? null : comparison ===
     null ? null : (
     <VStack alignItems={"left"}>
-      {comparison.compareFeeds.added.map((feed, index) => (
-        <FeedGroupView
-          key={index}
-          endpoint={comparison.endpointsNew[0]}
-          index={index}
-          feed={feed}
-          apiSpecifications={comparison.apiSpecsNew}
-          title={"Added"}
-          status={1}
-        />
-      ))}
-      {comparison.compareFeeds.removed.map((feed, index) => (
-        <FeedGroupView
-          key={index}
-          endpoint={comparison.endpointsOld[0]}
-          index={index}
-          feed={feed}
-          apiSpecifications={comparison.apiSpecsOld}
-          title={"Removed"}
-          status={2}
-        />
-      ))}
-      {comparison.compareFeeds.updated.map((feed, index) => (
-        <FeedCompareGroupView
-          key={index}
-          index={index}
-          feed={feed}
-          oldServers={comparison.apiSpecsOld}
-          newServers={comparison.apiSpecsNew}
-          title={"Updated"}
-          status={3}
-        />
-      ))}
-      {comparison.compareFeeds.unchanged.map((feed, index) => (
-        <FeedGroupView
-          key={index}
-          endpoint={comparison.endpointsNew[0]}
-          index={index}
-          feed={feed}
-          apiSpecifications={comparison.apiSpecsNew}
-          title={"Unchanged"}
-          status={0}
-        />
-      ))}
+      {
+        newOis.map((oi, j) => (
+          <VStack key={j} alignItems={"left"}>
+            <Text p={2} bgColor={COLORS.main} fontSize={"md"} fontWeight={"bold"}>
+              {oi.title}
+            </Text>
+
+            {comparison.compareFeeds.added.map((feed, index) => (
+              <FeedGroupView
+                key={index}
+                endpoint={comparison.endpointsNew[j]}
+                index={index}
+                feed={feed}
+                apiSpecifications={newOis[j].apiSpecifications}
+                title={"Added"}
+                status={1}
+              />
+            ))}
+            {comparison.compareFeeds.removed.map((feed, index) => (
+              <FeedGroupView
+                key={index}
+                endpoint={comparison.endpointsOld[j]}
+                index={index}
+                feed={feed}
+                apiSpecifications={oldOis[j].apiSpecifications}
+                title={"Removed"}
+                status={2}
+              />
+            ))}
+            {comparison.compareFeeds.updated.map((feed, index) => (
+              <FeedCompareGroupView
+                key={index}
+                index={index}
+                feed={feed}
+                newEndpoint={comparison.endpointsNew[j]}
+                oldEndpoint={comparison.endpointsOld[j]}
+                oldServers={oldOis[j].apiSpecifications}
+                newServers={newOis[j].apiSpecifications}
+                title={"Updated"}
+                status={3}
+              />
+            ))}
+            {comparison.compareFeeds.unchanged.map((feed, index) => (
+              <FeedGroupView
+                key={index}
+                endpoint={comparison.endpointsNew[j]}
+                index={index}
+                feed={feed}
+                apiSpecifications={newOis[j].apiSpecifications}
+                title={"Unchanged"}
+                status={0}
+              />
+            ))}
+          </VStack>
+        ))
+      }
+
     </VStack>
   );
 };
 
-export default Endpoint;
+export default CompareEndpoints;
--- frontend/src/Custom/FeedCompareGroupView.js
+++ frontend/src/Custom/FeedCompareGroupView.js
@@ -3,7 +3,7 @@ import { VStack } from "@chakra-ui/react";
 import ExpandableView from "./ExpandableView";
 import FeedCompareRowView from "./FeedCompareRowView";
 
-const FeedGroupView = ({ index, feed, oldServers, newServers, title, status }) => {
+const FeedGroupView = ({ index, feed, oldServers, newEndpoint, oldEndpoint, newServers, title, status }) => {
   return (
     <VStack key={index} alignItems={"left"} width={"100%"}>
       <ExpandableView
@@ -12,7 +12,7 @@ const FeedGroupView = ({ index, feed, oldServers, newServers, title, status }) =
           <VStack key={index} alignItems={"left"} width={"100%"}>
             <ExpandableView
               status={status}
-              view={<FeedCompareRowView feed={feed} oldServers={oldServers.servers} newServers={newServers.servers} />}
+              view={<FeedCompareRowView feed={feed} newEndpoint={newEndpoint} oldEndpoint={oldEndpoint} oldServers={oldServers.servers} newServers={newServers.servers} />}
               header={feed.newFeed.feed}
             />
           </VStack>
