3f383aebd1dc3b479f71a03182abb4de33444deb
hiletmis <hiletmis@gmail.com>
2023-10-29 18:32:57 +0300

    Updates

---
+++ static/js/react-syntax-highlighter_languages_refractor_hsts.782681ab.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkconfig_manager=self.webpackChunkconfig_manager||[]).push([[3140],{57761:function(a){function e(a){a.languages.hsts={directive:{pattern:/\b(?:includeSubDomains|max-age|preload)(?=[\s;=]|$)/i,alias:"property"},operator:/=/,punctuation:/;/}}a.exports=e,e.displayName="hsts",e.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_hsts.782681ab.chunk.js.map
