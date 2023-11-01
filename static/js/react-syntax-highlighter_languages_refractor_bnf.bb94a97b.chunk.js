c470eccbbe392c89a0ff61499883a3969eab023b
hiletmis <hiletmis@gmail.com>
2023-11-01 16:11:35 +0300

    Updates

--- static/js/react-syntax-highlighter_languages_refractor_bnf.bb94a97b.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkconfig_manager=self.webpackChunkconfig_manager||[]).push([[8497],{19784:function(n){function a(n){n.languages.bnf={string:{pattern:/"[^\r\n"]*"|'[^\r\n']*'/},definition:{pattern:/<[^<>\r\n\t]+>(?=\s*::=)/,alias:["rule","keyword"],inside:{punctuation:/^<|>$/}},rule:{pattern:/<[^<>\r\n\t]+>/,inside:{punctuation:/^<|>$/}},operator:/::=|[|()[\]{}*+?]|\.{3}/},n.languages.rbnf=n.languages.bnf}n.exports=a,a.displayName="bnf",a.aliases=["rbnf"]}}]);
-//# sourceMappingURL=react-syntax-highlighter_languages_refractor_bnf.bb94a97b.chunk.js.map
