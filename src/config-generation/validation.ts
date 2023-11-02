8a1690d9ff5505893f60d927b8163efe04398f08
metobom <unutkan2013@yandex.com>
2023-11-02 18:16:50 +0300

    Add validation schemas

---
+++ src/config-generation/validation.ts
@@ -0,0 +1,16 @@
+import { ethers } from 'ethers';
+import { z } from 'zod';
+import { readJson } from './config-utils';
+
+export const deploymentLocationsSchema = z.enum(['aws', 'local']).array().nonempty().max(2);
+export const evmAddressSchema = z
+  .string()
+  .refine((address) => ethers.utils.isAddress(address), { message: 'Airnode address is not in correct format!' });
+
+export const apiDataSchema = z.object({
+  alias: z.string(),
+  airnode: evmAddressSchema,
+  xpub: z.string(),
+  supportedFeedsInBatches: z.record(z.string(), z.array(z.array(z.string()))),
+  deploymentLocations: deploymentLocationsSchema
+});
