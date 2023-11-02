a4b44d2514bba50c0fb3e27f4a186279c1e8ab5e
hiletmis <hiletmis@gmail.com>
2023-11-02 14:03:42 +0300

    prettier

--- frontend/src/Custom/UnsupportedFeedsView.js
+++ frontend/src/Custom/UnsupportedFeedsView.js
@@ -3,55 +3,49 @@ import { checkSupportedFeedsInBatches } from '../Helpers/Utils';
 import TableView from './TableView';
 import ExpandableView from './ExpandableView';
 
-
 const fileIsMissing = () => {
-    return (
-        <VStack p={2} bgColor={"red"} borderRadius={'md'} alignItems={'left'} width={'100%'}>
-            <Flex>
-                <Text fontWeight={'bold'} color={"white"} fontSize={'md'}>
-                    API Data file is missing.
-                </Text>
-            </Flex>
-        </VStack>
-    )
-}
+  return (
+    <VStack p={2} bgColor={'red'} borderRadius={'md'} alignItems={'left'} width={'100%'}>
+      <Flex>
+        <Text fontWeight={'bold'} color={'white'} fontSize={'md'}>
+          API Data file is missing.
+        </Text>
+      </Flex>
+    </VStack>
+  );
+};
 
 const ListView = (table) => {
-    return (
-        <ExpandableView
-            status={2}
-            view={
-                <TableView
-                    parameters={table}
-                    headers={[
-                        { key: 'name', value: 'Name' },
-                        { key: 'status', value: 'Status' }
-                    ]}
-                />
-            }
-            header={'Unsupported Feeds'}
-        ></ExpandableView>
-    )
-}
+  return (
+    <ExpandableView
+      status={2}
+      view={
+        <TableView
+          parameters={table}
+          headers={[
+            { key: 'name', value: 'Name' },
+            { key: 'status', value: 'Status' }
+          ]}
+        />
+      }
+      header={'Unsupported Feeds'}
+    ></ExpandableView>
+  );
+};
 
 const UnsupportedFeedsView = ({ apiData, ois }) => {
+  try {
+    if (apiData === undefined || apiData == null) return fileIsMissing();
+    const result = checkSupportedFeedsInBatches(apiData.config.supportedFeedsInBatches, [ois])[0];
+    const table = result.unsupportedFeeds.map((feed, index) => ({
+      name: feed.feed,
+      status: 'Could not find feed in config file.'
+    }));
 
-    try {
-        if (apiData === undefined || apiData == null) return fileIsMissing();
-        const result = (checkSupportedFeedsInBatches(apiData.config.supportedFeedsInBatches, [ois]))[0]
-        const table = result.unsupportedFeeds.map((feed, index) => (
-            {
-                name: feed.feed,
-                status: "Could not find feed in config file."
-            }
-        ))
-
-        return apiData === undefined || result.unsupportedFeeds.length === 0 ? null :
-            <ListView table={table} />
-
-    } catch (error) {
-        console.log(error);
-    }
+    return apiData === undefined || result.unsupportedFeeds.length === 0 ? null : <ListView table={table} />;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export default UnsupportedFeedsView;
