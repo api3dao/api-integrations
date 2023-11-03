f91cc26b7501c0c063c246ba01391be4de6d28a5
bdrhn9 <bedirhan@api3.org>
2023-11-03 18:27:22 +0300

    Create script to generate test data for the endpoint `POST /getToken`

---
+++ backend/scripts/create-test-data.ts
@@ -0,0 +1,20 @@
+import { ethers } from 'ethers';
+import { signMessage } from '../src/evm';
+
+const main = async () => {
+  const wallet = ethers.Wallet.fromMnemonic(process.env.MNEMONIC);
+  const message = process.env.TOKEN_REQUEST_MESSAGE;
+  const timestamp = Date.now().toString();
+  const signature = await signMessage(wallet, message, timestamp);
+
+  console.log(JSON.stringify({ signature, message, timestamp }));
+};
+
+main()
+  .then(() => {
+    process.exit(0);
+  })
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  });
