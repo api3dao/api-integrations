d188f590b64bcdf17fb518e0f61d56fba463cc59
hiletmis <hiletmis@gmail.com>
2023-11-07 16:05:21 +0300

    Improve navigation title

--- frontend/src/Custom/ApiProvider.js
+++ frontend/src/Custom/ApiProvider.js
@@ -1,7 +1,35 @@
-import { Text, Image, VStack, Flex } from '@chakra-ui/react';
+import { Text, Image, VStack, Flex, Spacer, Stack } from '@chakra-ui/react';
+import { ArrowBackIcon, ChevronRightIcon } from '@chakra-ui/icons';
 import { COLORS, CONSTANTS } from '../data/constants';
 
-const ApiProvider = ({ deployment, setProvider }) => {
+const Path = ({ stack, onClick }) => {
+  if (stack === undefined || stack == null) {
+    return null;
+  }
+
+  return (
+    stack.length === 0 ? null :
+      <Flex width={"100%"} gap={3} alignItems={'center'}>
+        {
+          stack.map((item, index) => (
+            <Stack direction={"row"}>
+              <ChevronRightIcon width={'30px'} height={'30px'} cursor={'pointer'} />
+              <Text bgColor={"gray.100"} p={2} borderRadius={"xl"} key={index} fontSize={'sm'} >
+                {item}
+              </Text>
+            </Stack>
+          ))
+        }
+        <Spacer />
+        <ArrowBackIcon onClick={onClick} width={'30px'} height={'30px'} cursor={'pointer'} />
+      </Flex>
+  );
+
+};
+
+
+const ApiProvider = ({ deployment, setProvider, stack, onClick }) => {
+
   return deployment === undefined || deployment == null ? null : (
     <VStack cursor={'pointer'}>
       <Flex
@@ -28,6 +56,7 @@ const ApiProvider = ({ deployment, setProvider }) => {
         <Text fontSize={'md'} fontWeight={'semi-bold'}>
           {deployment.alias.toUpperCase()}
         </Text>
+        <Path stack={stack} onClick={onClick} />
       </Flex>
     </VStack>
   );
