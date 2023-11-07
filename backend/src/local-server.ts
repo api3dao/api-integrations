8d51ef78d4fedec06697e6c5c2b0de45af90c1d2
bdrhn9 <bedirhan@api3.org>
2023-11-07 16:43:41 +0300

    Update local development server

--- backend/src/local-server.ts
+++ backend/src/local-server.ts
@@ -1,7 +1,7 @@
 import * as dotenv from 'dotenv'; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 dotenv.config();
 import express from 'express';
-import { getToken } from './handlers';
+import { deploymentStatus, getToken } from './handlers';
 import type { APIGatewayProxyEvent } from 'aws-lambda';
 
 const { PORT } = process.env;
@@ -12,6 +12,13 @@ const app = express();
 app.post('/getToken', express.json(), async (req, res) => {
   const result = await getToken({
     body: JSON.stringify(req.body)
+  } as APIGatewayProxyEvent);
+  res.status(result.statusCode).header(result.headers).send(result.body);
+});
+
+app.get('/deploymentStatus', express.json(), async (req, res) => {
+  const result = await deploymentStatus({
+    queryStringParameters: req.query
   } as APIGatewayProxyEvent);
   res.status(result.statusCode).header(result.headers).send(result.body);
 });
