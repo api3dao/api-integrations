7fa9964de62075fafe130093dbcd7157f578828d
bdrhn9 <bedirhan@api3.org>
2023-11-08 14:34:43 +0300

    Assert environment variables as non-null

--- backend/scripts/create-test-data.ts
+++ backend/scripts/create-test-data.ts
@@ -2,8 +2,8 @@ import { ethers } from 'ethers';
 import { signMessage } from '../src/evm';
 
 const main = async () => {
-  const wallet = ethers.Wallet.fromMnemonic(process.env.MNEMONIC);
-  const message = process.env.TOKEN_REQUEST_MESSAGE;
+  const wallet = ethers.Wallet.fromMnemonic(process.env.MNEMONIC!);
+  const message = process.env.TOKEN_REQUEST_MESSAGE!;
   const timestamp = Date.now().toString();
   const signature = await signMessage(wallet, message, timestamp);
 
