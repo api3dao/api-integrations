7c44a5ddaef437c20a388bc8383bb3dbffe0711c
hiletmis <hiletmis@gmail.com>
2023-12-10 14:45:29 +0300

    Fix scrollbars in UI components

--- frontend/src/Components/Welcome.js
+++ frontend/src/Components/Welcome.js
@@ -12,7 +12,7 @@ const Welcome = () => {
   })(require.context('../../../data/apis/', true, /([a-z0-9-]+-airnode-feed.json|api-data.json)/));
 
   return (
-    <Flex spacing={4} height={'100vh'} overflow={'scroll'}>
+    <Flex spacing={4} height={'100vh'} overflow={'scroll'} className="hide-scrollbar">
       <VStack spacing={4} width={'100%'} alignItems={'center'} justifyItems={'stretch'}>
         {provider === null ? (
           <SearchApiProvider providers={configFiles} setProvider={setProvider} />
