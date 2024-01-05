15e581b01065423d7086d1d095450a99f13f52d9
metobom <unutkan2013@yandex.com>
2024-01-05 17:00:15 +0300

    Add the CI script

---
+++ src/ci-scripts/check-generated-files.ts
@@ -0,0 +1,27 @@
+import { globSync } from 'glob';
+import { readJson } from '../config-generation/config-utils';
+import { apisData } from '../generated/apis';
+import { apiDataSchema } from '../types';
+import { isEqual } from 'lodash';
+
+function main() {
+  const apiDataPaths = globSync('./data/apis/*/api-data.json').filter((pth) => !pth.includes('/demo/'));
+  const apiDataJson = {};
+  const airnodeMapping = {};
+  apiDataPaths.forEach((path) => {
+    const apiData = apiDataSchema.parse(readJson(path));
+    apiDataJson[apiData.alias] = apiData;
+    airnodeMapping[apiData.alias] = apiData.airnode;
+  });
+
+  const result = isEqual(apisData, apiDataJson);
+  if (!result) {
+    throw Error(
+      'src/generates/apis.ts\'s contents does not match the data/apis/*/apis-data.json. Did you forget to run "pnpm build"?'
+    );
+  }
+
+  console.log('apis.ts is sync.');
+}
+
+main();
