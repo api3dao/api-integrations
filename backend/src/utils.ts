8c317713dc8cef1c3483575267be2ca09552564b
bdrhn9 <bedirhan@api3.org>
2024-01-06 17:09:56 +0300

    Prevent to stringify stringified JSON object not to result escaped quotes

--- backend/src/utils.ts
+++ backend/src/utils.ts
@@ -2,13 +2,35 @@ import { COMMON_HEADERS } from './constants';
 
 import type { APIGatewayProxyEventHeaders, APIGatewayProxyResult } from 'aws-lambda';
 
+// This function helps to not JSON.stringify twice if the input is already stringified JSON
+// Otherwise the output would be a string with escaped quotes
+const jsonParseIfCan = (str: string): unknown => {
+  try {
+    return JSON.parse(str);
+  } catch (e) {
+    return str;
+  }
+};
+
 export const generateErrorResponse = (
   statusCode: number,
   message: string,
   detail?: string,
   causing?: string
 ): APIGatewayProxyResult => {
-  return { statusCode, headers: COMMON_HEADERS, body: JSON.stringify({ message, detail, causing }) };
+  return {
+    statusCode,
+    headers: COMMON_HEADERS,
+    body: JSON.stringify(
+      {
+        message: jsonParseIfCan(message),
+        detail: jsonParseIfCan(detail),
+        causing: jsonParseIfCan(causing)
+      },
+      null,
+      2
+    )
+  };
 };
 
 export const isAuthorized = (headers: APIGatewayProxyEventHeaders) => {
